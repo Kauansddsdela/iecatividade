c381cf42d97e318693e8d9309c12c55223660600
Kauang☯ <116198015+Kauansddsdela@users.noreply.github.com>
2024-11-21 17:53:55 -0300

    Add files via upload

---
+++ iec/consultacep.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+async function consultarCep(cep) {
+    try {
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+        console.log('Dados do CEP:', response.data);
+    } catch (error) {
+        console.error('Erro ao consultar o CEP:', error.message);
+        process.exit(1); // Indica falha no pipeline se houver erro
+    }
+}
+
+// Consultar um CEP de exemplo
+consultarCep('01001000');
